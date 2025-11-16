9a74f060615d564196180af62390f1c5d1191c87
Ahmad812002 <ahmad812002@gmail.com>
2025-11-16 16:14:58 +0100

    adding bcrypt on requirements.txt

--- test.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb+srv://ahmad812002_db_user:Hamada.CS812002@dana.51p0ug4.mongodb.net/vperfumes")
-db = client["vperfumes"]
-print(db.list_collection_names())
